989b9353871f0a099144c638b170229822644f63
Khoajzta <101576530+Khoajzta@users.noreply.github.com>
2025-11-05 20:21:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/quanlythuchi_android_firestore/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.quanlythuchi_android_firestore
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quanlythuchi_android_firestore.ui.theme.Quanlythuchi_android_firestoreTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Quanlythuchi_android_firestoreTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Quanlythuchi_android_firestoreTheme {
+        Greeting("Android")
+    }
+}
